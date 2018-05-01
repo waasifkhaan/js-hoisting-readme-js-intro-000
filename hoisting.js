2f7086b46d292eb6c229235ea3cae79607e6189d
Wasif Khan <waasif.khaan@gmail.com>
2018-05-01 16:19:13 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -4,23 +4,28 @@
   // console.log("but here's my number..");
   // console.log("so call me");
     return "maybe"
-    }
+  }
   
 
 var thisIsCrazy = function (){
     console.log("hey!!!");
-  }
+  
 }
 var crazy = function crazy() {
   // fix the code in here:
   thisIsCrazy();
+}
 
+ 
  function sayMy() {
+    var name;
+    
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+    
   } 
 
-var sayMyName = function sayMyName() {
+function sayMyName() {
   // fix the code in here:
   
 
